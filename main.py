be0a33a2ccd660ce67ecbc66496d7c62eb58e061
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2025-09-10 17:55:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+n1 = int(input("digite um número?"))
+n2 = int(input("digite um número?"))
+n3 = int(input("digite um número?"))
+n4 = int(input("digite um número?"))
+soma = n1 + n2
+print(soma)
+media = (n1 + n2 + n3 + n4)/4
+print(media)
+if n1>n2:
+    print(n1,"é mais velho que",n2)
+else:
+    print(n2,"é mais velho que",n1)
+nome = input(str("Seu nome?"))
+idade = input(str("qual sua idade?"))
+
+print(type(nome))
+print(type(idade))
+print(float("1"+"1"))
